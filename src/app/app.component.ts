9a9fc6f98794759276e47cfd07295350e6b8d741
Abdulrahman Mohamed <maanoatu@gmail.com>
2018-03-24 20:09:01 +0300

    up to date

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { Http, Response} from '@angular/http';
-
-import { Details } from './details';
 
 @Component({
   selector: 'app-root',
@@ -9,29 +6,5 @@ import { Details } from './details';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  userName='';
-  avatar_url='';
-  details:Details;
-
-  constructor(private http:Http){}
-
-  searchUser(){
-  	this.http.get('https://api.github.com/users/'+this.userName+ '?access_token=5cde94a9d7f6cb69965f644824a1e582cc9b3539').
-  	subscribe(
-  		(res:Response)=>{
-  			const details= res.json();
-  			console.log("details");
-        this.details=details;
-  		},
-      // (err:HttpErrorResponse) => { if (err.error instanceof Error){
-      //       console.log("Client-side Error occured");
-      //     }
-      //     else{console.log("An error has occured")
-      //       }
-      //     }
-  		)
-  }
-
-
-
+  title = 'app';
 }
